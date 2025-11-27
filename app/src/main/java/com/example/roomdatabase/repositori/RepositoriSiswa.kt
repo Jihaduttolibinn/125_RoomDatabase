f7dcecb30ef003002e1efaca9652edd7403d44dc
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-11-27 21:06:18 +0700

    SELESAI

---
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -0,0 +1,18 @@
+package com.example.roomdatabase.repositori
+
+import com.example.roomdatabase.room.Siswa
+import com.example.roomdatabase.room.SiswaDAO
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun InsertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDAO
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun InsertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
+
