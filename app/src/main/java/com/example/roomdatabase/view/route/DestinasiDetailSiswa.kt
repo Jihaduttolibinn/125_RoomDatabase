c12f76f73b47ca79c8dea44a907fcc75671669a0
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-12-04 16:58:54 +0700

    Menambahkan Overide val itemIdArg

--- app/src/main/java/com/example/roomdatabase/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -6,3 +6,4 @@ import com.example.roomdatabase.R
 object DestinasiDetailSiswa : DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
