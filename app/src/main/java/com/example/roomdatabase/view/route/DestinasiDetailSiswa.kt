0eb0fca99295ea4384caebf202b5fb63ae070a88
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-12-04 16:59:29 +0700

    Menambahkan val routeWithArgs

--- app/src/main/java/com/example/roomdatabase/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -7,3 +7,5 @@ object DestinasiDetailSiswa : DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
     const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
