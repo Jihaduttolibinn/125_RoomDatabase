1a0a7496bb4301ce0b4e93599ce88edb05ce00b7
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-12-04 16:47:40 +0700

    Menambahkan insert

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -1,9 +1,11 @@
 package com.example.roomdatabase.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,4 +15,7 @@ interface SiswaDAO {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa : Siswa)
-}
+
+
+
+
