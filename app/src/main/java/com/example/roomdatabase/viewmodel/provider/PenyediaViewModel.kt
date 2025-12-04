53874abed262028251b7656583c84777990b4a48
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-12-04 16:50:57 +0700

    Menambahkan initializer

--- app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.roomdatabase.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.DetailViewModel
 import com.example.roomdatabase.viewmodel.EntryViewModel
 import com.example.roomdatabase.viewmodel.HomeViewModel
 
@@ -13,11 +15,11 @@ object PenyediaViewModel{
         initializer {
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
-        }
-    }
-}
+
+
 
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
